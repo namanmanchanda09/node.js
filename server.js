13d81976e91c72ffd65eff11c37042f5eb9c0619
namanmanchanda09 <naman.mohan09@outlook.com>
2019-09-16 00:07:57 +0530

    serving statis files

--- server.js
+++ server.js
@@ -7,14 +7,18 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/:id',(req,res)=>{
-    console.log(req.query);   //access te query string for ex localhost:3000/?name=naman&age=20
-    //req.body
-    //req.header
-    console.log(req.params);
-    res.send('getting root');    
+// app.get('/:id',(req,res)=>{
+//     console.log(req.query);   //access te query string for ex localhost:3000/?name=naman&age=20
+//     //req.body
+//     //req.header
+//     console.log(req.params);  //access to parameters for ex localhost:3000/1234
+//     res.status(404).send('not found');   //sending the status
 
-});
+// });
+
+app.use(express.static(__dirname + '/public'));   //middleware for serving static files using express
+
+
 
 
 
