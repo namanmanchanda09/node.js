f7e342f6343b7879261dcfcda058b4b4aa1d4798
namanmanchanda09 <naman.mohan09@outlook.com>
2019-09-15 23:57:08 +0530

    about RestAPI

--- server.js
+++ server.js
@@ -7,19 +7,38 @@ const app = express();
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
-app.get('/profile',(req,res)=>{
-    res.send('hello boy');
-})
+app.get('/',(req,res)=>{
+    console.log(req.query);
+    res.send('getting root');    //access te query string for ex localhost:3000/?name=naman&age=20
+    //req.body
+    //req.header
+    //req.params
+});
 
-app.post('/profile',(req,res)=>{
-    console.log(req.body);
-    const user={
-        name:'Sally',
-        age:20
-    };
 
-    res.send(user);
-})
+
+
+
+
+
+
+
+
+
+
+
+// app.get('/profile',(req,res)=>{
+//     res.send('hello boy');
+// })
+// app.post('/profile',(req,res)=>{
+//     console.log(req.body);
+//     const user={
+//         name:'Sally',
+//         age:20
+//     };
+
+//     res.send(user);
+// })
 app.listen(3000);
 
 
